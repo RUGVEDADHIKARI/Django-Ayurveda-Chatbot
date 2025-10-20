d4a067585fa92b651664feaa3f591b694080c52f
Rugved Adhikari <141148110+RUGVEDADHIKARI@users.noreply.github.com>
2025-10-20 18:24:35 +0530

    Update settings.py

--- ayurveda_project/settings.py
+++ ayurveda_project/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-=h%1&+l1fuzzl(1z+#g6!u^4k!)0f$^3&vt+ax67-61e*!&*r2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'django-ayurveda-chatbot-1.onrender.com',  # new Render URL
+ALLOWED_HOSTS = [  
     'django-ayurveda-chatbot.onrender.com',    # (optional) old one
     'localhost',
     '127.0.0.1'
